84e14bca90023ec793f4f8d16b036b6528f85f27
panneer-m <m.p.samraj7@gmail.com>
2018-09-28 17:30:59 +0530

    click track event

--- app/routes/subscription.js
+++ app/routes/subscription.js
@@ -3,13 +3,11 @@ import Route from '@ember/routing/route';
 export default Route.extend({
     actions:{
         sub() {
-            window.fcWidget.open({ name: "subscription" });
+            //window.fcWidget.open();
+            
             window.fcWidget.track('cancel_subscription', {
                 //Key and value can be anything relatable to your web app.
-                cartValue: 100,
-                lastItemCategory: 'subscription'
               });
-            // alert("sam")
         }
     }
 });
