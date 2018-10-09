fc63a3f567b453195034caf5cd31eab150054611
panneer-m <m.p.samraj7@gmail.com>
2018-10-09 09:15:05 +0530

    cookie issues fixed

--- app/components/primary-header/component.js
+++ app/components/primary-header/component.js
@@ -20,7 +20,16 @@ export default Component.extend(commonEvent, {
         }
     },
     deletCookie(name) {
-        document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT';
+        // debugger;
+        let i = 0, domain = document.domain, parts = domain.split('.'),
+        tmpCookie = "_zg" + (new Date()).getTime();
+      while (i < (parts.length - 1) && document.cookie.indexOf(tmpCookie + '='
+        + tmpCookie) == -1) {
+        domain = parts.slice(-1 - (++i)).join('.');
+        document.cookie = tmpCookie + "=" + tmpCookie + ";domain=" + domain + ";";
+      }
+        // tmpCookie + "=;expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=" + domain + ";";
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:01 GMT;domain="+ domain + ";";
     }
 
 });
